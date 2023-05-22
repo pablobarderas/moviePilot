67f530d125f38df96f8511df192d8b5f80fed184
Marino Burillo Navarro <marino.burillo@metrica.es>
2023-05-22 14:29:07 +0200

    Empty Spring Boot project

---
+++ moviePilot/src/main/java/com/app/moviePilot/MoviePilotApplication.java
@@ -0,0 +1,12 @@
+package com.app.moviePilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviePilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviePilotApplication.class, args);
+	}
+}
