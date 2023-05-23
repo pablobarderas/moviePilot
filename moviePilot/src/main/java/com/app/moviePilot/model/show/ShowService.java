ef53d88cd0191bf4de3e9066c554b61a1c0ab07f
Your Name <you@example.com>
2023-05-23 10:46:06 +0200

    ShowService

--- moviePilot/src/main/java/com/app/moviePilot/model/show/ShowService.java
+++ moviePilot/src/main/java/com/app/moviePilot/model/show/ShowService.java
@@ -5,21 +5,27 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.app.moviePilot.controller.parser.ShowParser;
 import com.app.moviePilot.repository.ShowRepository;
 
 @Service
 public class ShowService {
 
     private final ShowRepository showRepository;
+    private final ShowParser showParser;
 
     @Autowired
-    public ShowService(ShowRepository showRepository) {
+    public ShowService(ShowRepository showRepository, ShowParser showParser) {
         this.showRepository = showRepository;
-    }
+        this.showParser = showParser;
+    }//end constructor
 
     public List<Show> getAllShows() {
         return showRepository.findAll();
-    }
+    }//end method
 
-    // Other methods in the service class
+    public List<Show> getPopularShows() {
+        return showRepository.findAll();
+    }//end method
+
 }//end class
