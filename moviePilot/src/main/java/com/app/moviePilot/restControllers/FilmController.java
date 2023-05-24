8fe60d87c263e302b580902bbee31a63fc2d3ed2
Pablo Barderas <Pablo Barderas@192.168.1.36>
2023-05-24 11:13:11 +0200

    update FilmController()

--- moviePilot/src/main/java/com/app/moviePilot/restControllers/FilmController.java
+++ moviePilot/src/main/java/com/app/moviePilot/restControllers/FilmController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.app.moviePilot.controller.parser.FilmParser;
 import com.app.moviePilot.model.film.Film;
@@ -16,6 +17,7 @@ import com.google.gson.JsonObject;
  *
  */
 @RestController
+@RequestMapping(value = "/movies")
 public class FilmController {
 
 	// GET POPULAR MOVIES
