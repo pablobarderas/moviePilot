bf04b31dbd7c2308feaea2e51cf1219cdeae6431
Pablo Barderas <Pablo Barderas@192.168.1.36>
2023-05-25 15:10:31 +0200

    Add searchController, movie search function and changes on getObjectByPage from FilmParser

--- moviePilot/src/main/java/com/app/moviePilot/repository/GenresRespository.java
+++ moviePilot/src/main/java/com/app/moviePilot/repository/GenresRespository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.app.moviePilot.model.enums.Genres;
 
+/**
+ * 
+ * @author Pablo Barderas
+ *
+ */
 public interface GenresRespository extends JpaRepository<Genres, Long>{
 
 }
