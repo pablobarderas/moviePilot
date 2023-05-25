c3b6324375d01fae92294e20d5f8cd3462bd2cc6
Pablo Barderas <Pablo Barderas@192.168.1.36>
2023-05-25 12:52:38 +0200

    add genresRepository file

---
+++ moviePilot/src/main/java/com/app/moviePilot/repository/GenresRespository.java
@@ -0,0 +1,9 @@
+package com.app.moviePilot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.moviePilot.model.enums.Genres;
+
+public interface GenresRespository extends JpaRepository<Genres, Long>{
+
+}
